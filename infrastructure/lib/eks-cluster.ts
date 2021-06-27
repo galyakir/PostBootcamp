68f249b5e2e03a881e43dabba6ca4da25bd35b6b
galyakir <gal.yakir@gmail.com>
2021-06-27 16:08:57 +0300

    add k8s ymls to deploy

--- infrastructure/lib/eks-cluster.ts
+++ infrastructure/lib/eks-cluster.ts
@@ -1,13 +1,11 @@
 import * as cdk from '@aws-cdk/core';
 import * as eks from '@aws-cdk/aws-eks';
-
 import  * as ec2  from '@aws-cdk/aws-ec2';
 
 
 export class eksStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-
         const cluster = new eks.Cluster(this, id, {
             version: eks.KubernetesVersion.V1_20,
             defaultCapacity: 0
@@ -17,9 +15,9 @@ export class eksStack extends cdk.Stack {
             minSize: 4,
             diskSize: 50,
         });
-        cluster.addHelmChart('NginxIngress', {
-            chart: 'nginx-ingress',
-            repository: 'https://helm.nginx.com/stable',
+        cluster.addHelmChart('nginx-ingress', {
+            chart: 'nginx-ingress-controller',
+            repository: 'https://charts.bitnami.com/bitnami',
         });
     }
 }
